e258e1c948005e968ccbc1c6e56e206c7b467993
abdoulfataho <yamyaogofataho@gmail.com>
2024-05-10 08:37:47 -0400

    I've declared variable and calculated the garden area

---
+++ app.js
@@ -0,0 +1,9 @@
+/*values of plants and areas*/
+const gardenRadius = 5;
+const plantRadius = 0.8;
+const plantsWeekOne = 20;
+const plantsWeekTwo = 40;
+const plantsWeekThree = 80;
+const PI = 3.1415;
+const gardenArea = PI * gardenRadius * gardenRadius;
+console.log( gardenArea)
