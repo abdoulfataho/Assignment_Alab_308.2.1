c6510c07b940c1562c8450d7215f10b1c7447bd0
abdoulfataho <yamyaogofataho@gmail.com>
2024-05-10 08:43:28 -0400

    Just wrote If statements to determine what to do in the garden

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const plantsWeekTwo = 40;
 const plantsWeekThree = 80;
 const PI = 3.1415;
 const gardenArea = PI * gardenRadius * gardenRadius;
-console.log( gardenArea)
+console.log( gardenArea);
+/*functions to find out what you should do with the garden.*/
+const plantArea = 0.8;
+if (plantArea > 0.8 * gardenArea)
+ {console.log("Prune")}
+ 
+if (plantArea >= 0.5 * gardenArea && plantArea <= 0.8 * gardenArea)
+ {console.log("Monitor")}
+ 
+if (plantArea < 0.5 * gardenArea)
+ {console.log("Plant")}
